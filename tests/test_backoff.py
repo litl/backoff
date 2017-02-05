4590738706da8c00de916f499771499713f13f75
Bob Green <bgreen@litl.com>
2017-02-05 08:08:37 -0500

    Move jitter tests to separate file
    No need to implement this set twice between sync and async.

--- tests/test_backoff.py
+++ tests/test_backoff.py
@@ -7,14 +7,6 @@ import random
 from tests.common import _log_hdlrs, _save_target
 
 
-def test_full_jitter():
-    for input in range(100):
-        for i in range(100):
-            jitter = backoff.full_jitter(input)
-        assert jitter >= 0
-        assert jitter <= input
-
-
 def test_expo():
     gen = backoff.expo()
     for i in range(9):
---
+++ tests/test_jitter.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+import backoff
+
+
+def test_full_jitter():
+    for input in range(100):
+        for i in range(100):
+            jitter = backoff.full_jitter(input)
+        assert jitter >= 0
+        assert jitter <= input
