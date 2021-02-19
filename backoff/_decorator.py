7daecc67b6c4652a7a684d3b6f54803c1476c9ae
Andrew Eckart <andrew.g.eckart@gmail.com>
2021-02-19 18:22:57 -0500

    Fix typo in docstring

--- backoff/_decorator.py
+++ backoff/_decorator.py
@@ -124,7 +124,7 @@ def on_exception(wait_gen,
             backoff.
         max_tries: The maximum number of attempts to make before giving
             up. Once exhausted, the exception will be allowed to escape.
-            The default value of None means their is no limit to the
+            The default value of None means there is no limit to the
             number of tries. If a callable is passed, it will be
             evaluated at runtime and its return value used.
         max_time: The maximum total amount of time to try for before
