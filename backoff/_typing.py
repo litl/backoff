36ba667cf58cb98d16f592b898ca9ad45a8255cb
Leonhard S <leonhard-sei@outlook.com>
2021-07-14 19:36:27 +0200

    Add decorator-specific detail and handler types

--- backoff/_typing.py
+++ backoff/_typing.py
@@ -1,12 +1,62 @@
 # coding:utf-8
 import logging
-from typing import Any, Callable, Dict, Generator, Sequence, Union, TypeVar
-
+import sys
+from typing import (Any, Callable, Dict, Generator, Sequence, Tuple, Union,
+                    TYPE_CHECKING, TypeVar)
 
 T = TypeVar("T")
 
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+elif TYPE_CHECKING:
+    try:
+        from typing_extensions import TypedDict
+    except ImportError:
+        pass
+try:
+    _ = TypedDict
+except NameError:
+    DetailsException = DetailsPredicate = Dict[str, Any]  # type: ignore
+else:
+    class _Details(TypedDict):
+        """Shared keys available for all invocation details."""
+        target: Callable[..., Any]
+        args: Tuple[Any, ...]
+        kwargs: Dict[str, Any]
+        tries: int
+        elapsed: float
+
+    class DetailsException(_Details):
+        """Invocation details for on_exception backoff handlers.
+
+        Availble keys:
+
+            `target` : reference to the function or method being invoked
+            `args`   : positional arguments to func
+            `kwargs` : keyword arguments to func
+            `tries`  : number of invocation tries so far
+            `elapsed`: elapsed time in seconds so far
+            `wait`   : seconds to wait
+        """
+        wait: float
+
+    class DetailsPredicate(_Details):
+        """Invocation details for on_predicate backoff handlers.
+
+        Availble keys:
+
+            `target` : reference to the function or method being invoked
+            `args`   : positional arguments to func
+            `kwargs` : keyword arguments to func
+            `tries`  : number of invocation tries so far
+            `elapsed`: elapsed time in seconds so far
+            `value`  : value triggering backoff
+        """
+        value: Any
+
 _CallableT = TypeVar('_CallableT', bound=Callable[..., Any])
-_Handler = Callable[[Dict[str, Any]], None]
+_HandlerPredicate = Callable[[DetailsPredicate], None]
+_HandlerDetails = Callable[[DetailsException], None]
 _Jitterer = Callable[[float], float]
 _MaybeCallable = Union[T, Callable[[], T]]
 _MaybeLogger = Union[str, logging.Logger]
